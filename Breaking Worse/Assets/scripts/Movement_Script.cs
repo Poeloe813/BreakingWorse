f62034d471a91bdd27efe3f0c320ca6de477b602
luckyman9000 <110716744+luckyman9000@users.noreply.github.com>
2024-10-09 12:14:00 +0200

    updated player

--- Breaking Worse/Assets/scripts/Movement_Script.cs
+++ Breaking Worse/Assets/scripts/Movement_Script.cs
@@ -1,69 +1,22 @@
-using System.ComponentModel.Design;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class Movement1 : MonoBehaviour
+public class Movement_Script : MonoBehaviour
 {
-    public InputAction moveRight;
-    public InputAction moveLeft;
-    public InputAction moveUp;
-    public InputAction moveDown;
 
+
+    public Camera Camera;
     private Rigidbody rb;
-
-    public float SpeedLeftRight;
-    public float SpeedUp;
-    public float SpeedDown;
-    public float SpeedSink;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        rb.velocity = new Vector3(0, 0, 0);
-
-        if (moveLeft.ReadValue<float>() == 1)
-        {
-            rb.velocity += new Vector3(-SpeedLeftRight, 0, 0);
-        }
-
-        else if (moveRight.ReadValue<float>() == 1)
-        {
-            rb.velocity += new Vector3(SpeedLeftRight, 0, 0);
-        }
-
-
-
-        if (moveUp.ReadValue<float>() == 1)
-        {
-            rb.velocity += new Vector3(0, SpeedUp, 0);
-        }
-
-        else if (moveDown.ReadValue<float>() == 1)
-        {
-            rb.velocity += new Vector3(0, -SpeedDown, 0);
-        }
-        else if (moveUp.ReadValue<float>() == 0)
-        {
-            rb.velocity += new Vector3(0, -SpeedSink, 0);
-        }
-    }
-
-    private void OnEnable()
-    {
-        moveRight.Enable();
-        moveLeft.Enable();
-        moveUp.Enable();
-        moveDown.Enable();
-    }
-    private void OnDisable()
-    {
-        moveRight.Disable();
-        moveLeft.Disable();
-        moveUp.Disable();
-        moveDown.Disable();
+        rb.rotation = new Quaternion(transform.rotation.x, Camera.transform.rotation.y, transform.rotation.z, transform.rotation.w).normalized;
     }
 }
