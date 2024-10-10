dafcb4b2b1770cc056804b1864c496bf3dcaf4c8
luckyman9000 <110716744+luckyman9000@users.noreply.github.com>
2024-10-10 15:24:08 +0200

    updated player

--- Breaking Worse/Assets/scripts/Movement_Script.cs
+++ Breaking Worse/Assets/scripts/Movement_Script.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement_Script : MonoBehaviour
 {
 
 
     public Camera Camera;
+    public float speed;
+
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -18,5 +22,12 @@ public class Movement_Script : MonoBehaviour
     void Update()
     {
         rb.rotation = new Quaternion(transform.rotation.x, Camera.transform.rotation.y, transform.rotation.z, transform.rotation.w).normalized;
+        Camera.transform.position = rb.position;
+        rb.transform.GetChild(0).rotation = Camera.transform.rotation;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        rb.velocity = rb.rotation * new Vector3(value.Get<Vector2>().x * speed, rb.velocity.y, value.Get<Vector2>().y * speed);
     }
 }
